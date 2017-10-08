20e657afef14cfab6d61253d7aeea76f92c95a16
ColeTrammer <cole.trammer@gmail.com>
2017-10-08 11:30:27 -0700

    qwerfgthjmk,l.4r454

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,12 +4,48 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-for (int i = 0; i < eggs.size(); i++) { 
-	String s = eggs.get(i);
-	System.out.println(s);
-}
-return 0;
+		for (int i = 0; i < eggs.size(); i++) { 
+			String s = eggs.get(i);
+
+			if(s== "cracked") {
+				System.out.println(i);
+				return i;
+
+			}
+
+		}
+
+		return -1;
 	}
-	
-	//Add other methods here
+
+	public static int countPearls(List<Boolean> oysters) {
+		int counter = 0;
+
+		for (int i = 0; i < oysters.size(); i++) { 
+			Boolean t = oysters.get(i);
+
+			if(t == true) {
+
+				counter=counter +1;
+
+			}
+		}
+		System.out.println(counter);
+		return counter;
+	}
+	public static Double findTallest(List<Double> Peeps) {
+		Double biggestnum = Peeps.get(0);
+		for (int i = 0; i < Peeps.size(); i++) { 
+			Double p = Peeps.get(i);
+			
+			if(biggestnum < p) {
+				biggestnum = p;
+
+			}
+
+
+		}
+		System.out.println(biggestnum);
+		return biggestnum;
+	}
 }
--- src/SearchingAndSortingAlgorithms/SearchingAndSortingAlgorithms.java
+++ src/SearchingAndSortingAlgorithms/SearchingAndSortingAlgorithms.java
@@ -14,31 +14,31 @@ public class SearchingAndSortingAlgorithms {
 	 **/
 
 	/* 1. Create a method that will find return the first index of a broken egg */
-	@Test
-	public void testSearch() {
-		List<String> eggs = Arrays.asList(new String[] { "whole", "whole", "whole", "whole", "cracked", "whole", "whole", "whole",
-				"cracked", "whole", "whole", "whole" });
-		assertEquals(4, Algorithms.findBrokenEgg(eggs));
-	}
+	//@Test
+	//public void testSearch() {
+		//List<String> eggs = Arrays.asList(new String[] { "whole", "whole", "whole", "whole", "cracked", "whole", "whole", "whole",
+		//		"cracked", "whole", "whole", "whole" });
+		//assertEquals(4, Algorithms.findBrokenEgg(eggs));
+//	}
 
 	/* 2. Count how many oysters contain pearls. */
-//	@Test
-//	public void testSearchAndAccumulate() throws Exception {
-//		List<Boolean> oysters = Arrays.asList(new Boolean[] { false, false, true, false, false, false, false, false, false, false, false,
-//				false, false, false, false, false, false, false, false, false, false, false, false, true });
-//		assertEquals(2, Algorithms.countPearls(oysters));
+	//@Test
+	//public void testSearchAndAccumulate() throws Exception {
+	//	List<Boolean> oysters = Arrays.asList(new Boolean[] { false, false, true, false, false, false, false, false, false, false, false,
+		//		false, false, false, false, false, false, false, false, false, false, false, false, true });
+	//	assertEquals(2, Algorithms.countPearls(oysters));
 //	}
 
 	/* 3. Create a method that will return the height of the tallest person. */
-//	@Test
-//	public void testFindMaxiumum() throws Exception {
-//		List<Double> peeps = Arrays.asList(new Double[] { 5.7, 6.2, 3.4, 6.2, 5.0, 5.5, 4.7, 6.2, 3.4, 4.2, 5.0, 4.5, 5.1, 6.6 });
-//		assertEquals(6.6, Algorithms.findTallest(peeps), 0.0);
-//	}
+	@Test
+	public void testFindMaxiumum() throws Exception {
+	List<Double> peeps = Arrays.asList(new Double[] { 5.7, 6.2, 3.4, 6.2, 5.0, 5.5, 4.7, 6.2, 3.4, 20.8, 4.2, 5.0, 4.5, 5.1, 6.6 });
+	assertEquals(20.8, Algorithms.findTallest(peeps), 0.0);
+	}
 
 	/* 4. Create a method that will find the longest word. */
 //	@Test
-//	public void testFindMaxiumumString() throws Exception {
+	//	public void testFindMaxiumumString() throws Exception {
 //		List<String> words = Arrays.asList(new String[] { "accoutrements", "acumen", "anomalistic", "auspicious", "bellwether",
 //				"callipygian", "circumlocution", "concupiscent", "conviviality", "coruscant", "cuddlesome", "cupidity", "cynosure",
 //				"ebullient", "equanimity", "excogitate", "gasconading", "idiosyncratic", "luminescent", "magnanimous", "nidificate",
