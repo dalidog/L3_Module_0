500921d0db007b10dadb4b1f711289aaec8bbe02
dalidog <league@WorthyMini-6.attlocal.net>
2017-10-29 11:29:29 -0700

    qegtjl;p9532efg

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		for (int i = 0; i < eggs.size(); i++) { 
+		for (int i = 0; i < eggs.size(); i++) {
 			String s = eggs.get(i);
 
-			if(s== "cracked") {
+			if (s == "cracked") {
 				System.out.println(i);
 				return i;
 
@@ -21,56 +21,77 @@ public class Algorithms {
 	public static int countPearls(List<Boolean> oysters) {
 		int counter = 0;
 
-		for (int i = 0; i < oysters.size(); i++) { 
+		for (int i = 0; i < oysters.size(); i++) {
 			Boolean t = oysters.get(i);
 
-			if(t == true) {
+			if (t == true) {
 
-				counter=counter +1;
+				counter = counter + 1;
 
 			}
 		}
 		System.out.println(counter);
 		return counter;
 	}
+
 	public static Double findTallest(List<Double> Peeps) {
 		Double biggestnum = Peeps.get(0);
-		for (int i = 0; i < Peeps.size(); i++) { 
+		for (int i = 0; i < Peeps.size(); i++) {
 			Double p = Peeps.get(i);
-			
-			if(biggestnum < p) {
+
+			if (biggestnum < p) {
 				biggestnum = p;
 
 			}
-
 
 		}
 		System.out.println(biggestnum);
 		return biggestnum;
 	}
-	public static String findLongestWord (List<String> words) {
+
+	public static String findLongestWord(List<String> words) {
 		String biggestword = words.get(0);
 		for (int i = 0; i < words.size(); i++) {
 			String w = words.get(i);
-			if(biggestword.length() < w.length()) {
+			if (biggestword.length() < w.length()) {
 				biggestword = w;
 			}
 		}
 		System.out.println(biggestword);
 		return biggestword;
 	}
+
 	public static Boolean containsSOS(List<String> message1) {
 		String sos = "... --- ...";
 		for (int i = 0; i < message1.size(); i++) {
-			String message =message1.get(i);
+			String message = message1.get(i);
 
-		if(message.equals(sos) ) {
-			System.out.println(true);
-			return true;
+			if (message.equals(sos)) {
+				System.out.println(true);
+				return true;
 
-		}}
-		 System.out.println(false);
+			}
+		}
+		System.out.println(false);
 		return false;
-
 	}
+
+	public static List<Double> sortScores(List<Double> results) {
+			for (int j = 0; j < results.size()-1; j++) {
+				for (int i = 0; i < results.size()-1; i++) {
+					Double b = results.get(i+1);
+					Double a = results.get(i);
+					Double c = null;
+					if (b < a) {
+						c=a;
+						a=b;
+						b=c;
+						results.set(i, a);
+						results.set(i+1, b);
+					}
+				}
+			}
+			System.out.println(results);
+			return results;
+		}
 }
